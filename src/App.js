5c2d2705914e2440ad945112823cbc7986211136
Ramesh3282 <ramesharthanari.a@gmail.com>
2023-05-09 20:15:44 +0530

    updated the app.js and css file

--- src/App.js
+++ src/App.js
@@ -1,151 +1,131 @@
 
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
-const productDetails=[
-  {
-    image:'images/camera.jpg',
-    productType:'Camera Product',
-    rating:[1,2,3],
-    price:'$40.00 - $80.00',
+  const productList=[
+    {
+      productImage:'images/camera.jpg',
+      productName:'Camera Product',
+      productRating:5,
+      productPrice:'$40.00 - $80.00',
+      },
+    {
+      productImage:'images/fancy.jpg',
+      productName:'Fancy Item',
+      productRating:2,
+      productPrice:'$20.00 $18.00',
     },
-  {
-    image:'images/fancy.jpg',
-    productType:'Fancy Item',
-    rating:[1,2],
-    price:'$20.00 $18.00',
-  },
-  {
-    image:'images/lens.jpg',
-    productType:'Camera Lens Item',
-    rating:[1,2,3,4,5],
-    price:'$50.00 $25.00',
-  },
-
-  {
-    image:'images/milk.jpg',
-    productType:'Popular Milk Item',
-    rating:[1,2,3,4],
-    price:'$50.00 $25.00',
-  },
-
-  {
-    image:'images/mobile.jpg',
-    productType:'Mobile Sale Item',
-    rating:[1,2,3,4,5],
-    price:'$50.00 $25.00',
+    {
+      productImage:'images/lens.jpg',
+      productName:'Camera Lens Item',
+      productRating:4,
+      productPrice:'$50.00 $25.00',
     },
-  {
-    image:'images/pineforest.jpg',
-    productType:'Pineforest Fancy Product',
-    rating:[1],
-    price:'$120.00 - $280.00',
-  },
-  {
-    image:'images/sdcard.jpg',
-    productType:'Special SDCard  Item',
-    rating:[1,2,3,4,5],
-    price:'$20.00 $18.00',
-  },
 
-  {
-    image:'images/toy.jpg',
-    productType:'Popular Toy Item',
-    rating:[1,2,3,],
-    price:'$40.00',
-  },
-  
+    {
+      productImage:'images/milk.jpg',
+      productName:'Popular Milk Item',
+      productRating:4,
+      productPrice:'$50.00 $25.00',
+    },
 
-]
+    {
+      productImage:'images/mobile.jpg',
+      productName:'Mobile Sale Item',
+      productRating:3,
+      productPrice:'$50.00 $25.00',
+      },
+    {
+      productImage:'images/pineforest.jpg',
+      productName:'Pineforest Fancy Product',
+      productRating:1,
+      productPrice:'$120.00 - $280.00',
+    },
+    {
+      productImage:'images/sdcard.jpg',
+      productName:'Special SDCard  Item',
+      productRating:5,
+      productPrice:'$20.00 $18.00',
+    },
+
+    {
+      productImage:'images/toy.jpg',
+      productName:'Popular Toy Item',
+      productRating:3,
+      productPrice:'$40.00',
+    },
+    
+
+  ]
+ const [cartVal,setCartVal]=useState(0)
   return (
     <div className="App">
-      <div className='topbtn'>
-        <Home/>
-        <About/>
-        <button id="product">Products</button>
-      </div>
-      <Cart/>
-      <br/>
-      <br/>
-      { productDetails.map((value,index)=>(
-         <Shop 
-          key={index}
-          id={index}
-          image={value.image}
-          productType={value.productType}
-          rating={value.rating}
-          price={value.price}
-        
-         />
-      ))}
-     
+      <h1>Cart:{cartVal}</h1>
+      {
+        productList.map((product,index)=>
+       <ProductCard 
+       key={index}
+       productImage={product.productImage}
+       productName={product.productName}
+       productRating={product.productRating}
+       productPrice={product.productPrice}
+       cartVal={cartVal}
+       setCartVal={setCartVal}
+       />
+        )
+      }
     
     </div>
   );
 }
 
-function Home(){
-  return(
-    <button id='home'>Home</button>
-  )
-  
-}
-
-function About(){
-  return(
-    <button id="about">About</button>
-  )
-  
-}
-
-function addCartClick(event){
-
-  const val=document.getElementById('cartval').innerHTML
-  const getId=event.target.id
-  document.getElementById('cartval').innerHTML= +val +1
-  document.getElementById(getId).disabled = true;
-  
-  document.getElementById("product_"+getId).innerHTML +=`  <button id=removecart${getId}  onClick="removeCart(${getId})">Remove from cart</button>`
-  
-}
-window.removeCart=function(id){
-  const val=document.getElementById('cartval').innerHTML
-  
-  document.getElementById('removecart'+id).remove();
-  document.getElementById('cartval').innerHTML= +val -1
-  document.getElementById(id).disabled = false;
-  document.getElementById(id).setAttribute("onClick","addCartClick");
-
-  
-}
 function Rating(props){
+  
  return(
   
-    <span class="fa fa-star checked"></span>
+    <span className="fa fa-star checked"></span>
+  
    
  )
 }
-function Shop(productList){
-  
-  return(
 
-   <div className='product' id={"product_"+productList.id}>
-    <img src={productList.image} alt="cart"/>
-    <p><h3>{productList.productType}</h3></p>
-    <p>{productList.rating.map(item => <Rating />)}</p>
-    <p>{productList.price}</p>
+function ProductCard({productImage,productName,productRating,productPrice,cartVal,setCartVal}){
+  const [btnshow,setBtnshow]=useState(true)
+ 
+  const addCartHandler=()=>{
+    setBtnshow(false)
+    setCartVal(cartVal+1)
+   
+  }
+  const removeCartHandler=()=>{
+    setBtnshow(!btnshow)
+    setCartVal(cartVal-1)
+   
+  }
+  return(
+   
+   <div className='product'>
+   
+    <img src={productImage} alt={productName}/>
+    <h3>{productName}</h3>
+    <p>{productPrice}</p>
+    <div className='rating'>
+    {[...Array(productRating)].map((x, i) =>
+    <p><Rating/></p>
+  )}
+  </div>
+
+    {btnshow? <button onClick={addCartHandler}>Add to cart</button>:
+
+    <button onClick={removeCartHandler}>Remove from cart</button>
+
+  }
   
-    <button id={productList.id} onClick={addCartClick}>Add to cart</button>
-    
    </div>
   )
   
 }
-function Cart(){
-  return(
-    <button id="cart">Cart <span id="cartval">0</span></button>
-  )
-  
-}
+
 export default App;
